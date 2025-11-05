cf97bc9ef40e483ddd8f0f3eb958464854545d78
cora_lyn <caroroemer@gmx.de>
2025-11-05 21:46:08 +0100

    Restructuring Assets

--- GlobalScripts/Constants.gd
+++ GlobalScripts/Constants.gd
@@ -47,40 +47,40 @@ const LEVELS = {
 }
 
 # MENUES
-const PATH_CREDIT_SCENE := "res://Components/UI/Menues/credit_scene.tscn"
-const PATH_FADE := "res://Components/UI/Menues/fade.tscn"
-const PATH_GAME_COMPLETED := "res://Components/UI/Menues/game_completed.tscn"
-const PATH_IN_GAME_SETTINGS := "res://Components/UI/Menues/InGameSettings.tscn"
-const PATH_LEVEL_SELECT := "res://Components/UI/Menues/Level_Select.tscn"
-const PATH_MAIN_MENU := "res://Components/UI/Menues/MainMenu.tscn"
-const PATH_SETTINGS := "res://Components/UI/Menues/Settings.tscn"
-const PATH_VOLUME_SLIDER := "res://Components/UI/Controls/VolumeSlider.tscn"
-const PATH_WIN_SCREEN := "res://Components/UI/Menues/win_screen.tscn"
+const PATH_CREDIT_SCENE := 		"res://Components/UI/Menues/credit_scene.tscn"
+const PATH_FADE := 				"res://Components/UI/Menues/fade.tscn"
+const PATH_GAME_COMPLETED := 	"res://Components/UI/Menues/game_completed.tscn"
+const PATH_IN_GAME_SETTINGS := 	"res://Components/UI/Menues/InGameSettings.tscn"
+const PATH_LEVEL_SELECT := 		"res://Components/UI/Menues/Level_Select.tscn"
+const PATH_MAIN_MENU := 		"res://Components/UI/Menues/MainMenu.tscn"
+const PATH_SETTINGS := 			"res://Components/UI/Menues/Settings.tscn"
+const PATH_VOLUME_SLIDER := 	"res://Components/UI/Controls/VolumeSlider.tscn"
+const PATH_WIN_SCREEN := 		"res://Components/UI/Menues/win_screen.tscn"
 
 # SFX SETTINGS
 const STEP_SOUND_PITCH_SCALES := [0.95, 0.96, 0.97, 0.98, 0.99, 1, 1.01, 1.02, 1.03, 1.04, 1.05]
 const STEP_SOUND_VOLUME_CHANGE := [6.0, 6.5, 7.0, 7.5, 8.0]
 
 # SFX PATHS
-const SFX_PATH_STEP := "res://Sounds/SFX/step.mp3"
-const SFX_POSSESS := ""
-const SFX_UNPOSSESS := ""
-const SFX_BUTTON_PRESSED := ""
-const SFX_BUTTON_UNPRESSED := ""
-const SFX_PUSH_MENU_BUTTON := "res://Sounds/SFX/push_menu_button.mp3"
+const SFX_PATH_STEP := 			"res://Sounds/SFX/step.mp3"
+const SFX_POSSESS := 			""
+const SFX_UNPOSSESS := 			""
+const SFX_BUTTON_PRESSED := 	""
+const SFX_BUTTON_UNPRESSED := 	""
+const SFX_PUSH_MENU_BUTTON := 	"res://Sounds/SFX/push_menu_button.mp3"
 
 # BGM PATHS
-const BGM_PATH_TITLE_THEME := "res://Sounds/BGM/title-track.mp3"
-const BGM_PATH_WINTER_THEME := "res://Sounds/BGM/BGM-Winter.mp3"
-const BGM_PATH_SUMMER_THEME := "res://Sounds/BGM/BGM-Summer.mp3"
+const BGM_PATH_TITLE_THEME := 	"res://Sounds/BGM/title-track.mp3"
+const BGM_PATH_WINTER_THEME := 	"res://Sounds/BGM/BGM-Winter.mp3"
+const BGM_PATH_SUMMER_THEME := 	"res://Sounds/BGM/BGM-Summer.mp3"
 
 # SPRITES
-const SPRITE_PATH_DOOR_OPEN := "res://Sprites/fence-gate-open.png"
-const SPRITE_PATH_DOOR_CLOSED := "res://Sprites/fence-gate-closed.png"
-const SPRITE_PATH_ICE_FLOOR := "res://Sprites/IceFloor/Ice-floor.png"
-const SPRITE_PATH_STICKY_BUTTON_UNPRESSED := "res://Sprites/button_unpressed.png"
-const SPRITE_PATH_STICKY_BUTTON_PRESSED := "res://Sprites/button_pressed.png"
-const SPRITE_PATH_PRESSURE_PLATE_UNPRESSED := "res://Sprites/pressure_plate_off.png"
-const SPRITE_PATH_PRESSURE_PLATE_PRESSED := "res://Sprites/pressure_plate_on.png"
-const SPRITE_PATH_TOGGLE_BUTTON_UNPRESSED := "res://Sprites/toggle-button-purple.png"
-const SPRITE_PATH_TOGGLE_BUTTON_PRESSED := "res://Sprites/toggle-button-orange.png"
+const SPRITE_PATH_DOOR_OPEN := 					"res://Assets/Doors/fence-gate-open.png"
+const SPRITE_PATH_DOOR_CLOSED := 				"res://Assets/Doors/fence-gate-closed.png"
+const SPRITE_PATH_ICE_FLOOR := 					"res://Assets/Winter Mechanics/Ice Floors/Ice-floor.png"
+const SPRITE_PATH_STICKY_BUTTON_UNPRESSED := 	"res://Sprites/button_unpressed.png"
+const SPRITE_PATH_STICKY_BUTTON_PRESSED := 		"res://Sprites/button_pressed.png"
+const SPRITE_PATH_PRESSURE_PLATE_UNPRESSED := 	"res://Sprites/pressure_plate_off.png"
+const SPRITE_PATH_PRESSURE_PLATE_PRESSED := 	"res://Sprites/pressure_plate_on.png"
+const SPRITE_PATH_TOGGLE_BUTTON_UNPRESSED := 	"res://Sprites/toggle-button-purple.png"
+const SPRITE_PATH_TOGGLE_BUTTON_PRESSED := 		"res://Sprites/toggle-button-orange.png"
