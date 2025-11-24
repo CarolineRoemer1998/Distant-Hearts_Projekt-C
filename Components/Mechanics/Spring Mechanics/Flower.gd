aa17cb724ea698852133a22a626fe0fb12138eb1
cora_lyn <caroroemer@gmx.de>
2025-11-24 14:04:25 +0100

    Added Soil and Seed Assets for growing Flowers

--- Components/Mechanics/Spring Mechanics/Flower.gd
+++ Components/Mechanics/Spring Mechanics/Flower.gd
@@ -2,7 +2,13 @@ extends StaticBody2D
 
 class_name Flower
 
+@onready var flower: AnimatedSprite2D = $Flower
+@onready var seed: Sprite2D = $Seed
 
 func _ready() -> void:
 	set_collision_layer_value(Constants.LAYER_BIT_FLOWER, true)
+	
+
+func grow():
+	
 	Signals.flower_grows.emit(self)
