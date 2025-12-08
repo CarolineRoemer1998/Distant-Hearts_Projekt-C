8c8b8829e8bf691bf871790b2c551f68e03f4015
cora_lyn <caroroemer@gmx.de>
2025-12-08 12:18:40 +0100

    Level Design Spring 5

--- Assets/Environments/Leaves etc/Leaf.gd
+++ Assets/Environments/Leaves etc/Leaf.gd
@@ -27,7 +27,7 @@ func _process(delta: float) -> void:
 
 func set_new_starting_pos(is_first: bool):
 	speed_scale = 1 + rnd.randf_range(-0.05, 0.05)
-	frame = rnd.randi_range(0,7)
+	frame = rnd.randi_range(0,3)
 	falling_speed = init_falling_speed+rnd.randi_range(-25,25)
 	start_pos = get_new_start_pos()
 	if not is_first:
