f0e03123758cb574c75f184e17ab9da5a7430139
cora_lyn <caroroemer@gmx.de>
2025-12-10 17:21:28 +0100

    Updated Level Select Screen

--- Components/UI/VFX/LeafManager.gd
+++ Components/UI/VFX/LeafManager.gd
@@ -9,11 +9,13 @@ func _ready() -> void:
 	if level.season == Constants.SEASON.Spring:
 		leaf = preload(Constants.ANIMATED_SPRITE_CHERRY_BLOSSOM)
 	if level.season == Constants.SEASON.Summer:
+		amount_leaves = 150
 		leaf = preload(Constants.ANIMATED_SPRITE_POLLEN)
 	if level.season == Constants.SEASON.Fall:
 		leaf = preload(Constants.ANIMATED_SPRITE_FALL_LEAVES)
 	if level.season == Constants.SEASON.Winter:
 		leaf = preload(Constants.ANIMATED_SPRITE_SNOWFLAKE)
+	print(amount_leaves)
 	for i in amount_leaves:
 		var new_leaf = leaf.instantiate()
 		add_child(new_leaf)
